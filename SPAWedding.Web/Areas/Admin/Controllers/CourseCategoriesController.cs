1a0f90b3f1c5d7b1ba82de050fa0689c93510dd3
User <User@DESKTOP-5IFU6NT>
2021-07-28 13:15:43 +0430

    Area/Admin/CourseCategoies and CourseComments and CourseHeadLines adn Courses Controller

---
+++ SPAWedding.Web/Areas/Admin/Controllers/CourseCategoriesController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Net;
+using System.Web.Mvc;
+using SPAWedding.Core.Models;
+using SPAWedding.Infrastructure.Repositories;
+
+namespace SPAWedding.Web.Areas.Admin.Controllers
+{
+    [Authorize]
+    public class CourseCategoriesController : Controller
+    {
+        private readonly CourseCategoriesRepository _repo;
+        public CourseCategoriesController(CourseCategoriesRepository repo)
+        {
+            _repo = repo;
+        }
+        // GET: Admin/CourseCategories
+        public ActionResult Index()
+        {
+            return View(_repo.GetAll());
+        }
+
+        // GET: Admin/CourseCategories/Create
+        public ActionResult Create()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Title")] CourseCategory courseCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                _repo.Add(courseCategory);
+                return RedirectToAction("Index");
+            }
+
+            return View(courseCategory);
+        }
+
+        // GET: Admin/CourseCategories/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CourseCategory courseCategory = _repo.Get(id.Value);
+            if (courseCategory == null)
+            {
+                return HttpNotFound();
+            }
+            return PartialView(courseCategory);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Title")] CourseCategory courseCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                _repo.Update(courseCategory);
+                return RedirectToAction("Index");
+            }
+            return View(courseCategory);
+        }
+
+        // GET: Admin/CourseCategories/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CourseCategory courseCategory = _repo.Get(id.Value);
+            if (courseCategory == null)
+            {
+                return HttpNotFound();
+            }
+            return PartialView(courseCategory);
+        }
+
+        // POST: Admin/CourseCategories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            _repo.Delete(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ SPAWedding.Web/Areas/Admin/Controllers/CourseCommentsController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SPAWedding.Infrastructure.Repositories;
+using SPAWedding.Core.Models;
+using System.Net;
+using SPAWedding.Web.ViewModels;
+
+namespace SPAWedding.Web.Areas.Admin.Controllers
+{
+    [Authorize]
+    public class CourseCommentsController : Controller
+    {
+        private readonly CourseCommentsRepository _repo;
+        public CourseCommentsController(CourseCommentsRepository repo)
+        {
+            _repo = repo;
+        }
+        public ActionResult Index(int CourseId)
+        {
+            ViewBag.CourseName = _repo.GetCourseName(CourseId);
+            ViewBag.CourseId = CourseId;
+            var comments = _repo.GetCourseComments(CourseId);
+            var commentsVm = new List<CourseCommentWithPersianDateViewModel>();
+            foreach (var comment in comments)
+            {
+                var commentVm = new CourseCommentWithPersianDateViewModel(comment);
+                commentsVm.Add(commentVm);
+            }
+            return View(commentsVm);
+        }
+
+        public ActionResult Create(int CourseId)
+        {
+            ViewBag.CourseId = CourseId;
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CourseComment comment)
+        {
+            if (ModelState.IsValid)
+            {
+                comment.AddedDate = DateTime.Now;
+                _repo.Add(comment);
+                return RedirectToAction("Index", new { CourseId = comment.CourseId });
+            }
+            ViewBag.CourseId = comment.CourseId;
+            return View(comment);
+        }
+        public ActionResult AnswerComment(int CourseId, int parentCommentId)
+        {
+            ViewBag.CourseId = CourseId;
+            ViewBag.ParentId = parentCommentId;
+            return PartialView();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AnswerComment(CourseComment comment)
+        {
+            var user = _repo.GetCurrentUser();
+            comment.Name = user != null ? $"{user.FirstName} {user.LastName}" : "ادمین";
+            comment.Email = user != null ? user.Email : "-";
+            comment.AddedDate = DateTime.Now;
+            _repo.Add(comment);
+            return RedirectToAction("Index", new { CourseId = comment.CourseId });
+        }
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CourseComment comment = _repo.Get(id.Value);
+            if (comment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(comment);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(CourseComment comment)
+        {
+            if (ModelState.IsValid)
+            {
+                _repo.Update(comment);
+                return RedirectToAction("Index", new { CourseId = comment.CourseId });
+            }
+            return View(comment);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CourseComment comment = _repo.Get(id.Value);
+            if (comment == null)
+            {
+                return HttpNotFound();
+            }
+
+            var commentVm = new CourseCommentWithPersianDateViewModel(comment);
+            return PartialView(commentVm);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var CourseId = _repo.Get(id).CourseId;
+            _repo.DeleteComment(id);
+            return RedirectToAction("Index", new { CourseId });
+        }
+    }
+}
