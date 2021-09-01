5546e39af5984a1ff15b93e4882d432d21ef7dad
User <User@DESKTOP-5IFU6NT>
2021-09-01 17:24:24 +0430

    Infrastructure\Helpers\HierarchyLoop => create GetCourseCategoryHierarchy and GetCourseCategoryHierarchyForProducts methods for hierarchy in Course categories

--- MaryamRahimiFard.Infrastructure/Helpers/HierarchyLoop.cs
+++ MaryamRahimiFard.Infrastructure/Helpers/HierarchyLoop.cs
@@ -61,5 +61,58 @@ namespace MaryamRahimiFard.Infrastructure.Helpers
 
             return content;
         }
+
+        public static string GetCourseCategoryHierarchy(CourseCategory entity, int? selectedItemParent = null, int? selectedItem = null)
+        {
+            selectedItemParent = selectedItemParent ?? 0;
+            selectedItem = selectedItem ?? 0;
+            var content = string.Empty;
+            if (entity.Id == selectedItemParent)
+                content += "<li data-jstree='{ \"selected\" : true,\"opened\": true }' id=\"pg_" + entity.Id + "\">" + entity.Title;
+            else if (entity.Id == selectedItem)
+                content += "<li data-jstree='{ \"disabled\" : true }' id=\"pg_" + entity.Id + "\">" + entity.Title;
+            else
+                content += $"<li id='pg_{entity.Id}'>{entity.Title}";
+
+            if (entity.Children.Any())
+            {
+                entity.Children.ToList().ForEach(item =>
+                {
+                    if (item.IsDeleted == false)
+                    {
+                        content += "<ul>" + GetCourseCategoryHierarchy(item, selectedItemParent, selectedItem) + "</ul>";
+                    }
+                });
+            }
+            content += "</li>";
+
+            return content;
+        }
+        public static string GetCourseCategoryHierarchyForProducts(CourseCategory entity, int? selectedItemParent = null, int? selectedItem = null)
+        {
+            selectedItemParent = selectedItemParent ?? 0;
+            selectedItem = selectedItem ?? 0;
+            var content = string.Empty;
+            if (entity.Id == selectedItemParent)
+                content += "<li onclick=\"getCourseCategoryFeatures(" + entity.Id + ")\" data-jstree='{ \"selected\" : true,\"opened\": true }' id=\"pg_" + entity.Id + "\">" + entity.Title;
+            else if (entity.Id == selectedItem)
+                content += "<li onclick=\"getCourseCategoryFeatures(" + entity.Id + ")\" data-jstree='{ \"disabled\" : true }' id=\"pg_" + entity.Id + "\">" + entity.Title;
+            else
+                content += $"<li onclick=\"getCourseCategoryFeatures({entity.Id})\" id='pg_{entity.Id}'>{entity.Title}";
+
+            if (entity.Children.Any())
+            {
+                entity.Children.ToList().ForEach(item =>
+                {
+                    if (item.IsDeleted == false)
+                    {
+                        content += "<ul>" + GetCourseCategoryHierarchyForProducts(item, selectedItemParent, selectedItem) + "</ul>";
+                    }
+                });
+            }
+            content += "</li>";
+
+            return content;
+        }
     }
 }
